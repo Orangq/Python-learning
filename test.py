8c3483c41d5bda375bcde01429d7057dc00a5e06
cgq <403724813@qq.com>
2020-04-04 17:43:18 +0800

    s

--- test.py
+++ test.py
@@ -30,3 +30,35 @@ d.sayName()
 d.run()
 
 f = '3'
+
+
+class Animal:
+    def __init__(self, name):
+        self._name = name
+
+    @property
+    def name(self):
+        print('%s拿到了~~~~' % self._name)
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        self._name = name
+        print('设置%s成功了~~~~' % self._name)
+
+    def run(self, distance):
+        print('%s奔跑了%d米！！！' % (self._name, distance))
+
+    def sleep(self):
+        print('%s睡觉！！！' % self._name)
+
+
+class Dog(Animal):
+    pass
+
+
+dog = Dog('狗')
+dog.run(3)
+r = isinstance(dog, object)
+g = issubclass(Animal, object)
+print(g)
