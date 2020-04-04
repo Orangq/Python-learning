ed10f257863fbf421d43e8193747bc9f27491d9d
cgq <403724813@qq.com>
2020-04-04 10:10:27 +0800

    s

--- test.py
+++ test.py
@@ -2,3 +2,29 @@ a = 5
 print(a)
 a = 'str'
 print(len(a))
+
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def sayName(self):
+        '''
+      这是一个展示名字的方法
+      '''
+        print(self.name)
+
+    def run(self):
+        '''
+      奔跑
+      '''
+        print('%s快乐的奔跑' % self.name)
+
+
+b = Person('cheng')
+c = Person('sun')
+d = Person('guo')
+b.sayName()
+c.sayName()
+d.sayName()
+d.run()
